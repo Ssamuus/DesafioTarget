3c3c288901f71fcb0bc4d4d4201f1dfe76977b81
Samuel Oliveira <162358081+Ssamuus@users.noreply.github.com>
2024-09-18 17:12:16 -0300

    Create Ex5.py

---
+++ Ex5.py
@@ -0,0 +1,13 @@
+"""
+5) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. 
+Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. 
+Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+
+Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?  
+
+-Respota: 1º: Ligar dois interrupatores
+          2º: Ir para a outra sala, a lâmpada apagada é do interruptor que está desligado;
+          3º: Desligar um dos interruptores ligados;
+          4º: Ir novamente para a outra sala, a lâmpada apagada que anteriomente estava acessa é do interruptor
+              que foi desligado por último e a única lâmpada acessa é do único interruptor ligado.
+"""
